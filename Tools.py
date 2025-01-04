df3d3a2a521b0ae5432308c829a5001b2e77e409
Lootchy <zcolucci@gaming.tech>
2025-01-04 17:49:18 +0100

    Point Update

--- Tools.py
+++ Tools.py
@@ -23,15 +23,15 @@ class Tools:
         x_points = np.array([point[0] for point in pointList])
         y_points = np.array([point[1] for point in pointList])
 
-        # Créer des valeurs x pour tracer la courbe
-        x_vals = np.linspace(min(x_points)-1, max(x_points)+1, 500)
-        
         plt.figure(figsize=(12, 10))
         
         # Tracer l'interpolation pour chaque groupe successif de 3 points
         for i in range(0, len(x_points) - 2, 2): 
             x_subset = x_points[i:i+3]
             y_subset = y_points[i:i+3]
+            
+            # Restreindre les valeurs de x à l'intervalle du 1er au dernier point
+            x_vals = np.linspace(min(x_subset), max(x_subset), 100)
             
             # Calculer les valeurs y pour les valeurs de x dans x_vals
             y_vals_subset = [self.lagrange_interpolation(x, x_subset, y_subset) for x in x_vals]
